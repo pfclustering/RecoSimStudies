a73ccb46801408ed46ac3fdb37825778154115ca
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-11-26 11:34:12 +0100

    dictionary with ECAL tags to override

---
+++ Dumpers/test/ECALproductionHelper/cmsDrivers/override_ECAL_tags.py
@@ -0,0 +1,26 @@
+override_tags = {}
+
+override_tags[2021] = {}
+override_tags[2021]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL180fb_v1_mc'
+override_tags[2021]['EcalPedestalsRcd'] = 'EcalLaserAPDPNRatios_TL180fb_v1_mc'
+override_tags[2021]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL180fb_v1_mc'
+override_tags[2021]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL180fb_v1_mc'
+override_tags[2021]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL180fb_v1_mc'
+override_tags[2021]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL180fb_v1_mc'
+override_tags[2021]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL180fb_v1_mc'
+###override_tags[2021]['EcalPFRecHitThresholdsRcd'] = 'EcalPFRecHitThresholds_UL_2018_2e3sig'
+override_tags[2021]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
+override_tags[2021]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+
+override_tags[2023] = {}
+override_tags[2023]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL450fb_v1cor_mc'
+override_tags[2023]['EcalPedestalsRcd'] = 'EcalPedestals_TL450fb_v1_mc'
+override_tags[2023]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL450fb_v2_mc'
+override_tags[2023]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL450fb_v1_mc'
+override_tags[2023]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL450fb_v1_mc'
+override_tags[2023]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL450fb_v1cor_mc'
+override_tags[2023]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL450fb_v1cor_mc'
+###override_tags[2023]['EcalPFRecHitThresholdsRcd'] = 'EcalPFRecHitThresholds_TL450_mixed'
+override_tags[2023]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
+override_tags[2023]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+
