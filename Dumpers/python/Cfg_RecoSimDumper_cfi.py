c785d271f29786a3bc6dba4ed9eaaffae865c14d
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-04-06 16:12:13 +0200

    modifications to config files

--- Dumpers/python/Cfg_RecoSimDumper_cfi.py
+++ Dumpers/python/Cfg_RecoSimDumper_cfi.py
@@ -1,18 +1,34 @@
 import FWCore.ParameterSet.Config as cms
 
+import Geometry.CaloEventSetup.caloTowerConstituents_cfi 
+CaloTowerConstituentsMapBuilder = cms.ESProducer("CaloTowerConstituentsMapBuilder",
+   MapFile = cms.untracked.string('Geometry/CaloTopology/data/CaloTowerEEGeometric.map.gz')
+   )
+
 recosimdumper = cms.EDAnalyzer("RecoSimDumper",
 
+    rhoCollection                   = cms.InputTag("fixedGridRhoAll"),
+    vertexCollection                = cms.InputTag("offlinePrimaryVertices"),
     genParticleCollection             = cms.InputTag("genParticles",""),
     caloParticleCollection            = cms.InputTag("mix","MergedCaloTruth"),
-    vertexCollection                  = cms.InputTag("offlinePrimaryVertices"),
     ebRechitCollection                = cms.InputTag("ecalRecHit","EcalRecHitsEB","RECO"),
     eeRechitCollection                = cms.InputTag("ecalRecHit","EcalRecHitsEE","RECO"),
     pfRechitCollection                = cms.InputTag("particleFlowRecHitECAL","","RECO"),
     pfClusterCollection               = cms.InputTag("particleFlowClusterECAL","","RECO"),
     ebSuperClusterCollection          = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALBarrel","RECO"), 
     eeSuperClusterCollection          = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALEndcapWithPreshower","RECO"), 
-    puInfoTag                         = cms.InputTag("slimmedAddPileupInfo","","RECO"), 
-    rhoTag                            = cms.InputTag("fixedGridRhoFastjetAll","","RECO"), 
+    useHcalTowers                   = cms.bool(False,  #compute HoE
+    hcalTowersCollection            = cms.InputTag("towerMaker"), #compute HoE
+    useRetunedSC                    = cms.bool(False),  #run on new RetunedSCs
+    useDeepSC                       = cms.bool(False),  #run on new DeepSCs
+    #ebRetunedSuperClusterCollection = cms.InputTag("particleFlowSuperClusterECALNewParams","particleFlowSuperClusterECALBarrelMustacheNewParams","RECO"), 
+    #eeRetunedSuperClusterCollection = cms.InputTag("particleFlowSuperClusterECALNewParams","particleFlowSuperClusterECALEndcapWithPreshowerMustacheNewParams","RECO"),
+    #ebDeepSuperClusterCollection    = cms.InputTag("particleFlowDeepSuperClusterECAL","particleFlowDeepSuperClusterECALBarrel","RECO"), 
+    #eeDeepSuperClusterCollection    = cms.InputTag("particleFlowDeepSuperClusterECAL","particleFlowDeepSuperClusterECALEndcapWithPreshower","RECO"),
+    #ebDeepSuperClusterLWPCollection = cms.InputTag("particleFlowDeepSuperClusterLWPECAL","particleFlowDeepSuperClusterLWPECALBarrel","RECO"), 
+    #eeDeepSuperClusterLWPCollection = cms.InputTag("particleFlowDeepSuperClusterLWPECAL","particleFlowDeepSuperClusterLWPECALEndcapWithPreshower","RECO"),
+    #ebDeepSuperClusterTWPCollection = cms.InputTag("particleFlowDeepSuperClusterTWPECAL","particleFlowDeepSuperClusterTWPECALBarrel","RECO"), 
+    #eeDeepSuperClusterTWPCollection = cms.InputTag("particleFlowDeepSuperClusterTWPECAL","particleFlowDeepSuperClusterTWPECALEndcapWithPreshower","RECO"),    
     
     doCompression                     = cms.bool(True),  #do the compression of floats
     nBits                             = cms.int32(23),   #nbits for float compression (<=23)
@@ -27,6 +43,8 @@ recosimdumper = cms.EDAnalyzer("RecoSimDumper",
     saveSuperCluster                  = cms.bool(False),  #save superClusters information
     saveShowerShapes                  = cms.bool(False),  #save showerShapes information
     saveScores                        = cms.bool(True),  #save scores information
+    scoreType                       = cms.string("simScore_final_combination"),  #score to be used for caloParticle matching if saveScores false
+    #scoreType                      = cms.string("sim_fraction"),  #score to be used for caloParticle matching
     genID                             = cms.vint32(22,11, -11), #save only caloParticles with this pdgId 
     #genID                            = cms.vdouble(0),  #save only caloParticles with this pdgId 
 
