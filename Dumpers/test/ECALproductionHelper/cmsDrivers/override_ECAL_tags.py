072978b6a31219007d9b83adfce0f71537714a21
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-06-02 15:52:21 +0200

    updated conditions for various lumis

--- Dumpers/test/ECALproductionHelper/cmsDrivers/override_ECAL_tags.py
+++ Dumpers/test/ECALproductionHelper/cmsDrivers/override_ECAL_tags.py
@@ -1,26 +1,85 @@
 override_tags = {}
 
-override_tags[2021] = {}
-override_tags[2021]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL180fb_v1_mc'
-override_tags[2021]['EcalPedestalsRcd'] = 'EcalPedestals_TL180fb_Model190fb_mc'
-override_tags[2021]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL180fb_v1_mc'
-override_tags[2021]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL180fb_v1_mc'
-override_tags[2021]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL180fb_v1_mc'
-override_tags[2021]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL180fb_v1_mc'
-override_tags[2021]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL180fb_v1_mc'
-###override_tags[2021]['EcalPFRecHitThresholdsRcd'] = 'EcalPFRecHitThresholds_UL_2018_2e3sig'
-override_tags[2021]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
-override_tags[2021]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+override_tags[150] = {}
+override_tags[150]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_UL_2018_mc_3sigma_v2'
+override_tags[150]['EcalPedestalsRcd'] = 'EcalPedestals_UL_2018_mc'
+override_tags[150]['EcalSRSettingsRcd'] = 'EcalSRSettings_beam2018_option1_mc'
+override_tags[150]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstantsRcd'
+override_tags[150]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstants_MC_Digi_2018'
+override_tags[150]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_UL_2018_mc'
+override_tags[150]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_UL_2018_mc'
+###override_tags[150]['EcalPFRecHitThresholdsRcd'] = ''
+override_tags[150]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_UL_2018_v1_mc' # std for MC
+override_tags[150]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_2017_mc' # alpha = 1
 
-override_tags[2023] = {}
-override_tags[2023]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL450fb_v1cor_mc'
-override_tags[2023]['EcalPedestalsRcd'] = 'EcalPedestals_TL450fb_v1_mc'
-override_tags[2023]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL450fb_v2_mc'
-override_tags[2023]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL450fb_v1_mc'
-override_tags[2023]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL450fb_v1_mc'
-override_tags[2023]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL450fb_v1cor_mc'
-override_tags[2023]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL450fb_v1cor_mc'
-###override_tags[2023]['EcalPFRecHitThresholdsRcd'] = 'EcalPFRecHitThresholds_TL450_mixed'
-override_tags[2023]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
-override_tags[2023]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+override_tags[180] = {}
+override_tags[180]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL180fb_v1_mc'
+override_tags[180]['EcalPedestalsRcd'] = 'EcalPedestals_TL180fb_Model190fb_mc'
+override_tags[180]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL180fb_v1_mc'
+override_tags[180]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL180fb_v1_mc'
+override_tags[180]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL180fb_v1_mc'
+override_tags[180]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL180fb_v1_mc'
+override_tags[180]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL180fb_v1_mc'
+###override_tags[180]['EcalPFRecHitThresholdsRcd'] = 'EcalPFRecHitThresholds_UL_2018_2e3sig'
+override_tags[180]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
+override_tags[180]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+
+override_tags[235] = {}
+override_tags[235]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL235fb_v1_mc'
+override_tags[235]['EcalPedestalsRcd'] = 'EcalPedestals_TL235fb_v1_mc'
+override_tags[235]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL235fb_v1_mc'
+override_tags[235]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL235fb_v1_mc'
+override_tags[235]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL235fb_v1_mc'
+override_tags[235]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL235fb_v1_mc'
+override_tags[235]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL235fb_v1_mc'
+###override_tags[235]['EcalPFRecHitThresholdsRcd'] = ''
+override_tags[235]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
+override_tags[235]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+
+override_tags[315] = {}
+override_tags[315]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL315fb_v1_mc'
+override_tags[315]['EcalPedestalsRcd'] = 'EcalPedestals_TL315fb_v1_mc'
+override_tags[315]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL315fb_v1_mc'
+override_tags[315]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL315fb_v1_mc'
+override_tags[315]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL315fb_v1_mc'
+override_tags[315]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL315fb_v1_mc'
+override_tags[315]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL315fb_v1_mc'
+###override_tags[315]['EcalPFRecHitThresholdsRcd'] = ''
+override_tags[315]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
+override_tags[315]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+
+override_tags[400] = {}
+override_tags[400]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL400fb_v1_mc'
+override_tags[400]['EcalPedestalsRcd'] = 'EcalPedestals_TL400fb_v1_mc'
+override_tags[400]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL400fb_v1_mc'
+override_tags[400]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL400fb_v1_mc'
+override_tags[400]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL400fb_v1_mc'
+override_tags[400]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL400fb_v1_mc'
+override_tags[400]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL400fb_v1_mc'
+###override_tags[400]['EcalPFRecHitThresholdsRcd'] = ''
+override_tags[400]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
+override_tags[400]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+
+override_tags[450] = {}
+override_tags[450]['EcalLaserAPDPNRatiosRcd'] = 'EcalLaserAPDPNRatios_TL450fb_v1cor_mc'
+override_tags[450]['EcalPedestalsRcd'] = 'EcalPedestals_TL450fb_v1_mc'
+override_tags[450]['EcalSRSettingsRcd'] = 'EcalSRSettings_TL450fb_v2_mc'
+override_tags[450]['EcalIntercalibConstantsMCRcd'] = 'EcalIntercalibConstantsMC_TL450fb_v1_mc'
+override_tags[450]['EcalIntercalibConstantsRcd'] = 'EcalIntercalibConstants_TL450fb_v1_mc'
+override_tags[450]['EcalTPGLinearizationConstRcd'] = 'EcalTPGLinearizationConst_TL450fb_v1cor_mc'
+override_tags[450]['EcalTPGPedestalsRcd'] = 'EcalTPGPedestals_TL450fb_v1cor_mc'
+###override_tags[450]['EcalPFRecHitThresholdsRcd'] = 'EcalPFRecHitThresholds_TL450_mixed'
+override_tags[450]['EcalADCToGeVConstantRcd'] = 'EcalADCToGeVConstant_2010_V2_Bon_mc' # std for MC
+override_tags[450]['EcalLaserAlphasRcd'] = 'EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1' # alpha = 1
+
+for r in ['EcalLaserAPDPNRatiosRcd', 'EcalPedestalsRcd', 'EcalSRSettingsRcd', 'EcalIntercalibConstantsMCRcd', 'EcalIntercalibConstantsRcd', 'EcalTPGLinearizationConstRcd', 'EcalTPGPedestalsRcd', 'EcalADCToGeVConstantRcd', 'EcalLaserAlphasRcd']:
+
+  print '\n ==> Checking record = {}'.format(r) 
+  
+  for i in [150,180,235,315,400,450]:
+  
+    print '  TL{}: {}'.format(i,override_tags[i][r])
+
+
+
 
